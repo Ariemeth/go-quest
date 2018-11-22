1ac83e2d4922ab54ad4858cc216658dcb1b50ee8
Edward Carmack <ariemethacc@gmail.com>
2018-11-22 02:17:15 -0500

    added initial character and auxilliary stubs

--- character.go
+++ character.go
@@ -1,1 +1,27 @@
-package goquest
+package main
+
+type player struct {
+	Campaign  campaign
+	Class     string
+	Equipment struct {
+		Head      equipment
+		Face      equipment
+		Body      equipment
+		Arms      equipment
+		Legs      equipment
+		Cloak     equipment
+		Shoulders equipment
+		Ring1     equipment
+		Ring2     equipment
+		Necklace  equipment
+		Weapon    equipment
+	}
+	Experience uint64
+	Gold       uint64
+	Level      uint64
+	MaxHP      uint64
+	MaxMP      uint64
+	Name       string
+	Spellbook  Spells
+	Stats      stats
+}
---
+++ stats.go
@@ -0,0 +1,10 @@
+package main
+
+type stats struct {
+	Charisma     uint
+	Constitution uint
+	Dexterity    uint
+	Intelligence uint
+	Strength     uint
+	Wisdom       uint
+}
