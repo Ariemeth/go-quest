c79b072c71d719f17a24d02c5e798248dbc2605d
Edward Carmack <ariemethacc@gmail.com>
2018-11-23 19:26:35 -0500

    added weapon test case

--- equipment/weapon_test.go
+++ equipment/weapon_test.go
@@ -28,6 +28,11 @@ func Test_generateWeapon(t *testing.T) {
 			args: args{level: 10},
 			want: 10,
 		},
+		{
+			name: "Level above the number of base weapons",
+			args: args{level: len(weaponBase) + 1},
+			want: len(weaponBase) + 1,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -36,7 +41,7 @@ func Test_generateWeapon(t *testing.T) {
 				t.Errorf("generateWeapon() Name field is empty and shouldn't be")
 			}
 			if (got.Bonus + got.Level) > tt.want {
-				t.Errorf("generateWeapon() total weapon level = %d, but should not be over %d", got.Bonus+got.Level, tt.args.level)
+				t.Errorf("generateWeapon() total weapon level = %d, but should not be over %d", got.Bonus+got.Level, tt.want)
 			}
 		})
 	}
